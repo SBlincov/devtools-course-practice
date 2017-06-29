7372e46e0ae0aa834c30ee6c49b8c91e26b0dbfa
SBlincov <sblin180@gmail.com>
2017-06-29 23:27:30 +0300

    Tests deleted, code redesigned

--- modules/cost_manager/test/test_Costs.cpp
+++ modules/cost_manager/test/test_Costs.cpp
@@ -1,10 +1,1 @@
-#include <gtest/gtest.h>
 
-#include "include/Costs.h"
-
-TEST(SBlincov_CostManager, MakeNewList) {
-    // Arrange
-    // Act
-    // Assert
-    ASSERT_NO_FATAL_FAILURE(Costs check(1000));
-}
