e96c74d0ccc1b7e8a4b6f64ca7b37b083a8c8a89
SBlincov <sblin180@gmail.com>
2017-06-30 23:56:00 +0300

    application.h created

---
+++ modules/polinom_calculator/include/application.h
@@ -0,0 +1,23 @@
+// Copyright 2017 SBlincov
+
+#ifndef MODULES_POLINOM_CALCULATOR_INCLUDE_HEAD_H_
+#define MODULES_POLINOM_CALCULATOR_INCLUDE_HEAD_H_
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include "Head.h"
+#include "Monom.h"
+
+class Application {
+ public:
+    Application();
+    std::string operator()(int argc, const char** argv);
+
+ private:
+    void Help(const char* appname, const char* message = "");
+    std::vector<std::string> ParseArguments(int argc, const char** argv);
+    std::string message_;
+};
+
+#endif  //  MODULES_POLINOM_CALCULATOR_INCLUDE_HEAD_H_
